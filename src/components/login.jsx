9881e0d56b3a58909e02c0c7067e9455adfe9aeb
AC244 <ac03basketball24@gmail.com>
2024-11-21 11:02:13 -0600

    styling for login and signup complete

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import * as authService from '../services/authService'; 
+import * as authService from '../services/authService';
 import '../app.css'
 
-const Login = ({setUser}) => {
+const Login = ({ setUser }) => {
   const navigate = useNavigate()
   const [message, setMessage] = useState([''])
   const [formData, setFormData] = useState({
@@ -33,65 +33,46 @@ const Login = ({setUser}) => {
 
 
   return (
-  //   <div className="auth-container">
-  //     <Link to="/" className="home-link">
-  //       <button className="auth-button">Home</button>
-  //     </Link>
-  //     <div className="auth-box">
-  //       <h2>Login</h2>
-  //       <form>
-  //         <input type="email" placeholder="Enter your email" className="auth-input" />
-  //         <input type="password" placeholder="Enter your password" className="auth-input" />
-  //         <button type="button" onClick={handleLogin} className="auth-button">
-  //           Login
-  //         </button>
-  //       </form>
-  //       <p>
-  //         Don't have an account?{' '}
-  //         <a href="/signup" className="auth-link">
-  //           Sign Up
-  //         </a>
-  //       </p>
-  //     </div>
-  //   </div>
-  // )
-  <main className="auth-container">
-  <h1>Log In</h1>
-  <p>{message}</p>
-  <form autoComplete="off" onSubmit={handleSubmit}>
-    <div>
-      <label htmlFor="email">Username:</label>
-      <input
-        type="text"
-        autoComplete="off"
-        id="username"
-        value={formData.username}
-        name="username"
-        onChange={handleChange}
-      />
-    </div>
-    <div>
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        autoComplete="off"
-        id="password"
-        value={formData.password}
-        name="password"
-        onChange={handleChange}
-      />
-    </div>
-    <div>
-      <button>Log In</button>
-      <Link to="">
-        <button>Cancel</button>
-      </Link>
-      <Link to={'/signup'}>Create an Account</Link>
-    </div>
-  </form>
-  
-</main>
-)
+    <main className="auth-container">
+      <div className="auth-box">
+
+        <h1>Log In</h1>
+        <p>{message}</p>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              autoComplete="off"
+              id="username"
+              value={formData.username}
+              name="username"
+              onChange={handleChange}
+              className="auth-input"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              autoComplete="off"
+              id="password"
+              value={formData.password}
+              name="password"
+              onChange={handleChange}
+              className="auth-input"
+            />
+          </div>
+          <div>
+            <button className="auth-button" >Log In</button>
+            <Link to={'/signup'} className="auth-link" >Create an Account</Link>
+          </div>
+        </form>
+      </div>
+
+
+    </main>
+  )
 }
 
 export default Login
